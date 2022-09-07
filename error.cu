45d45acd875317211a6abfa89a28def4fad6a1be
justinmgarrigus <justin.m.garrigus@gmail.com>
2022-09-07 18:39:23 -0500

    Improved readability of output

--- error.cu
+++ error.cu
@@ -1,5 +1,7 @@
 #include <stdio.h> 
 
+#define TEST_VALUE -12345 
+
 class ConstructorExample {
 public:
 	int var;
@@ -10,7 +12,6 @@ public:
 	
 	__host__ __device__ ConstructorExample(const ConstructorExample& other) {
 		var = other.var; 
-		printf("Copy constructor invoked\n"); 
 	}
 };
 
@@ -25,13 +26,16 @@ __global__ void kernel_sub(ConstructorExample obj) {
 __global__ void kernel_sub(char obj_serialized[sizeof(ConstructorExample)]) {
 	ConstructorExample obj = *(static_cast<ConstructorExample*>((void*)obj_serialized));
 	
-	printf("Sub-kernel invoked: %d\n", obj.var); 
+	printf("Sub-kernel invoked\n");
+	if (obj.var != TEST_VALUE) {
+		printf("Error: passed object not serialized correctly (%d)\n", obj.var); 
+	}
 }
 
 #endif 
 
 __global__ void kernel_base(ConstructorExample obj) {
-	printf("Base-kernel invoked: %d\n", obj.var); 
+	printf("Base-kernel invoked\n"); 
 	
 #if defined(DEVICE_COPY_CONSTRUCTOR_ERROR)
 
@@ -49,11 +53,13 @@ __global__ void kernel_base(ConstructorExample obj) {
 }
 
 int main(void) {
-	ConstructorExample obj1(-12345); 
-	printf("Obj1: %d\n", obj1.var); 
-	
+	ConstructorExample obj1(TEST_VALUE); 
 	ConstructorExample obj2 = obj1; 
-	printf("Obj2: %d\n", obj2.var); 
+	
+	if (obj1.var != obj2.var) {
+		printf("Error: copy-constructor not being invoked (%d != %d)\n", obj1.var, obj2.var); 
+		exit(1); 
+	}
 	
 	printf("Starting kernel example...\n"); 
 	kernel_base<<<1,1>>>(obj1);
@@ -61,7 +67,7 @@ int main(void) {
 	cudaError_t launch_error = cudaPeekAtLastError(); 
 	cudaError_t synchronize_error = cudaDeviceSynchronize(); 
 	if (launch_error != cudaSuccess || synchronize_error != cudaSuccess) {
-		fprintf(stderr, "Error in kernel: %s\n", cudaGetErrorString(launch_error != cudaSuccess ? launch_error : synchronize_error)); 
+		printf("Error in kernel: %s\n", cudaGetErrorString(launch_error != cudaSuccess ? launch_error : synchronize_error)); 
 		exit(1); 
 	}
 	
