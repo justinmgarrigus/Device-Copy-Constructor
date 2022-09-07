2faf2001c51d5975985f49e65693645dabfc04f4
justinmgarrigus <justin.m.garrigus@gmail.com>
2022-09-07 18:47:22 -0500

    Added comments

--- error.cu
+++ error.cu
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 
+// Used to verify that the data is serialized correctly device-side
 #define TEST_VALUE -12345 
 
 class ConstructorExample {
@@ -10,6 +11,8 @@ public:
 		this->var = var; 
 	}
 	
+	// Since this class has a copy constructor, you cannot pass it 
+	// to a device-side kernel launch.
 	__host__ __device__ ConstructorExample(const ConstructorExample& other) {
 		var = other.var; 
 	}
@@ -17,13 +20,17 @@ public:
 
 #if defined (DEVICE_COPY_CONSTRUCTOR_ERROR)
 
+// This is what we would normally do, but ConstructorExample has a copy
+// constructor so this will yield an error. 
 __global__ void kernel_sub(ConstructorExample obj) {
 	printf("Sub-kernel invoked: %d\n", obj.var); 
 }
 
 #else 
 
-__global__ void kernel_sub(char obj_serialized[sizeof(ConstructorExample)]) {
+// Instead, pass a byte array containing the data of the class we want
+// to pass. 
+__global__ void kernel_sub(char *obj_serialized) {
 	ConstructorExample obj = *(static_cast<ConstructorExample*>((void*)obj_serialized));
 	
 	printf("Sub-kernel invoked\n");
@@ -38,11 +45,13 @@ __global__ void kernel_base(ConstructorExample obj) {
 	printf("Base-kernel invoked\n"); 
 	
 #if defined(DEVICE_COPY_CONSTRUCTOR_ERROR)
-
+	
+	// Since obj has a copy constructor, this will yield an error. 
 	kernel_sub<<<1,1>>>(obj); 
 
 #else
 	
+	// Copy the data to a byte array and pass that instead. 
 	char *serialized = (char*)malloc(sizeof(obj)); 
 	memcpy(serialized, &obj, sizeof(obj)); 
 	kernel_sub<<<1,1>>>(serialized); 
@@ -54,7 +63,7 @@ __global__ void kernel_base(ConstructorExample obj) {
 
 int main(void) {
 	ConstructorExample obj1(TEST_VALUE); 
-	ConstructorExample obj2 = obj1; 
+	ConstructorExample obj2 = obj1; // Copy constructor invoked
 	
 	if (obj1.var != obj2.var) {
 		printf("Error: copy-constructor not being invoked (%d != %d)\n", obj1.var, obj2.var); 
